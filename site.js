ec6af54d464612f6485ed27425f498e480d36acc
Jeffrey Dean <59969527+j-j-dean@users.noreply.github.com>
2020-04-01 12:37:19 -0400

    Update site.js
    made updates

--- site.js
+++ site.js
@@ -1,11 +1,19 @@
 
+// Constants
+var jconstants = {
+   TOTAL_QUESTIONS: 30,
+   NUM_ANSWERS_PER_QUESTION: 4,
+   BONUS_SCORE: 4000,
+   ONE_SECOND_MS: 1000,  // milliseconds
+   INITIAL_TIMER_SECS: 45
+}
+
+// Jeopardy state variables
 var jeopardy = {
    completed_questions: 0,
    your_score: 0,
    data: [],
    random_questions: [],
-   TOTAL_QUESTIONS: 30,
-   BONUS_SCORE: 4000,
    time_remaining: 0
 };
 jeopardy.random_questions = new Array(6);
@@ -13,25 +21,29 @@ for (var i=0; i<6; i++) {
     jeopardy.random_questions[i] = new Array(5);
 }
 
+// Timer function to limit amount time to answer question
 jeopardy.setTimer = function() {
     if (jeopardy.time_remaining == 0) {
         var correct_answer = jeopardy.GetCorrectAnswer();
         jeopardy.BackToJeopardy("Time expired!", correct_answer);
+
+        // remove the click handler for the submit button
+        $("#final-answer").off( "click", jeopardy.CheckAnswer);
     } else {
         $('#timer').text("Time remaining: " + jeopardy.time_remaining.toString());
         jeopardy.time_remaining--;
         jeopardy.timeout = setTimeout(function() {
             jeopardy.setTimer();
-        }, 1000);
+        }, jconstants.ONE_SECOND_MS);
     }
 }
 
 // Get the correct answer to the question
 jeopardy.GetCorrectAnswer = function() {
 
-    // Get the column and row number of the question
-    col_num = $('#the-question').attr("topic");
-    row_num = $('#the-question').attr("question");
+    // Get the column and row number of the question from #the-question attributes
+    col_num = $('#the-question').attr("col_num");
+    row_num = $('#the-question').attr("row_num");
 
     var correct_answer = "";
     for (var i=0; i<jeopardy.data[col_num-1].
@@ -49,14 +61,14 @@ jeopardy.GetCorrectAnswer = function() {
     return correct_answer;
 }
 
-// Check the selected answer to the Jeopardy question
+// Check the selected answer for correctness (update the score, display message to user)
 jeopardy.CheckAnswer = function (){
 
     var msg="";
 
     // Get the column and row number of the question
-    col_num = $('#the-question').attr("topic");
-    row_num = $('#the-question').attr("question");
+    col_num = $('#the-question').attr("col_num");
+    row_num = $('#the-question').attr("row_num");
 
     // Get the correct answer from the data
     var correct_selection = jeopardy.data[col_num-1].
@@ -64,19 +76,20 @@ jeopardy.CheckAnswer = function (){
     var correct_answer=""
    
     // Get the user's selection
-    var selection = $('#answers').find(':checked').val();
+    var selection = $('#question-and-answers').find(':checked').val();
 
     // Update the score if the correct answer was given
     if (selection == correct_selection) {
-        jeopardy.your_score += parseInt($('#T'+col_num+'-Q'+row_num+'-label').text());
-        var score = $('#T'+col_num+'-Q'+row_num+'-label').text();
+
+        var query_string = ".points[col_num="+col_num+"][row_num="+row_num+"]";
+        jeopardy.your_score += parseInt($(query_string).text());
         msg = "Correct!"
     } else {
         msg = "Try Again!";
         correct_answer = jeopardy.GetCorrectAnswer();
     }
 
-    // reset the click handler to null
+    // remove the click handler
     $("#final-answer").off( "click", jeopardy.CheckAnswer);
 
     // return to the jeopardy grid view
@@ -89,17 +102,21 @@ jeopardy.UpdateRandomQuestions = function() {
     for (var i=0; i<jeopardy.data.length; i++) {
         var questions = jeopardy.data[i].Questions;
         var questions_length = questions.length;
+
+        // create question_number array based on available number of questions
         var question_number = [questions_length];
         for (j=0; j<questions_length; j++) {
             question_number[j] = j;
         }
+
+        // place the randomly selected questions into random_question array
         for (j=0; j<questions_length; j++) {
             var random = Math.floor(Math.random()*question_number.length);
 
             // move random number into answer array
             jeopardy.random_questions[i][j] = question_number[random];
 
-            // remove random entry from question_number
+            // remove random entry from question_number array
             var index = question_number.indexOf(question_number[random]);
             question_number.splice(index, 1);
         }
@@ -110,17 +127,10 @@ jeopardy.UpdateRandomQuestions = function() {
 jeopardy.DisplayCompletionMsg = function() {
     $('#completion-box').css("display", "block");
     $('#jeopardy-container').css("display", "none");
-
-    if (jeopardy.your_score >= jeopardy.BONUS_SCORE) {
-        $('#completion').find('h2').text("You completed the BONUS challenge!");
-
-        var text_string="print a copy of the screen or remember "
-        text_string += "the winning code to receive a prize when we return to class. ";
-        $('#extra').text(text_string);
-        text_string = "<h3 id='bonus'>Extra Bonus: memorize John3:16 or Matthew 22:38-39 for an additional prize</h3>";
-        $('#extra').append(text_string);
-        $('#bonus').css("color", "yellow");
+    if (jeopardy.your_score >= jconstants.BONUS_SCORE) {
+        $('#bonus').css("display", "block");
         $('#img-bonus').css("display", "block");
+        $('#challenge').css("display", "none");
     }
 }
 
@@ -160,20 +170,21 @@ jeopardy.GetQuestionsJSON = function() {
 // Return back to the jeopardy grid
 jeopardy.BackToJeopardy = function(msg, correct_answer) {
 
+    // Clear the grid view and replace with the question view
     $('#jeopardy-container').css("display", "block");
     $('#question-box').css("display", "none");
 
-    col_num = $('#the-question').attr("topic");
-    row_num = $('#the-question').attr("question");
-
-    var query_element = "#T" + col_num + "-Q" + row_num + "-label";
+    // Get the column and row number from the attributes
+    col_num = $('#the-question').attr("col_num");
+    row_num = $('#the-question').attr("row_num");
 
     // update the score on the display
     $('#score').text("Your score is " + jeopardy.your_score.toString() + " - " + msg);
     $('#timer').text(correct_answer);
 
-    // clear the last question and answers from the data object - question has been completed
-    $(query_element).text("");
+    // clear the last question and answers from the jeopardy grid view - question has been completed
+    var query_string = ".points[col_num="+col_num+"][row_num="+row_num+"]";
+    $(query_string).text("");
     jeopardy.data[col_num-1].Questions[jeopardy.random_questions[col_num-1][row_num-1]].Question = "";
     for (var i=0; i<jeopardy.data[col_num-1].
                     Questions[jeopardy.random_questions[col_num-1][row_num-1]].Answers.length; i++) {
@@ -183,8 +194,8 @@ jeopardy.BackToJeopardy = function(msg, correct_answer) {
     // increment the number of completed questions
     jeopardy.completed_questions++;
 
-    // check to see if all the questions have been completed
-    if (jeopardy.completed_questions == jeopardy.TOTAL_QUESTIONS) {
+    // display completion page if all questions completed
+    if (jeopardy.completed_questions == jconstants.TOTAL_QUESTIONS) {
         $('#score').text(msg + " All complete! Final score: " + jeopardy.your_score.toString());
         $('#timer').text(correct_answer);
         jeopardy.DisplayCompletionMsg();
@@ -195,7 +206,7 @@ jeopardy.BackToJeopardy = function(msg, correct_answer) {
 }
 
 // Replace the jeopardy grid view with the selected question and answers
-jeopardy.AskQuestion = function(box_element) {
+jeopardy.AskQuestion = function(points_element) {
 
     // Issue error message if data was not successfully read from server
     if (jeopardy.data.length == 0) {
@@ -203,11 +214,11 @@ jeopardy.AskQuestion = function(box_element) {
         return;
     }
 
-    // Retrieve the column and row number of the selected question
-    var col_num = box_element.attr("topic");
-    var row_num = box_element.attr("question");
+    // Get the column and row number of the selected question
+    var col_num = points_element.attr("col_num");
+    var row_num = points_element.attr("row_num");
 
-    // Skip question boxes that have already been completed
+    // Skip question boxes that have already been completed (question still in list)
     if (jeopardy.data[col_num-1].
           Questions[jeopardy.random_questions[col_num-1][row_num-1]].Question.length > 0) {
         
@@ -216,52 +227,46 @@ jeopardy.AskQuestion = function(box_element) {
                                 Questions[jeopardy.random_questions[col_num-1][row_num-1]].Question;
 
         // Retrieve the corresponding answers to the question, and add as radio buttons
-        var answers = "";
-        answers+="<br /><br />"
         var num_answers = jeopardy.data[col_num-1].
                                 Questions[jeopardy.random_questions[col_num-1][row_num-1]].Answers.length;
-        var checked="checked"
-        for (var i=0; i<num_answers; i++) {
-            var answer_selection = jeopardy.data[col_num-1].
-                    Questions[jeopardy.random_questions[col_num-1][row_num-1]].Answers[i].Selection;
-            answers += '<input type="radio" name="answers" value=' + 
-                                answer_selection + ' '+ checked + '/>'
-            checked = ""
-            answers += "&nbsp; &nbsp;"
-            answers += jeopardy.data[col_num-1].
-                            Questions[jeopardy.random_questions[col_num-1][row_num-1]].Answers[i].Selection;
-            answers += ")&nbsp;&nbsp;";
-            answers += jeopardy.data[col_num-1].
-                                Questions[jeopardy.random_questions[col_num-1][row_num-1]].Answers[i].Answer;
-            answers += "<br />";
-         }
 
-        // replace the question grid with the question and answer selection
-        $('#question-box').css("display", "block");
-        $('#jeopardy-container').css("display", "none");
+        // Check to see the right number of questions in the input data
+        if (num_answers != jconstants.NUM_ANSWERS_PER_QUESTION) {
+            $('#score').text("Inconsistent input file values.  Please check.");
+            return;
+        }
 
-        // update the font size
-        $('#the-question').css("font-size", "3vw");
-
-        // display the question
+        // Add the question to the question and answers view
         $('#the-question').text(question);
 
-        // add the answers
-        $('#answers').text("");
-        $('#answers').append(answers);
+        // Add the answers to the question and answers view
+        for (var i=0; i<num_answers; i++) {
+            var answer_selection = jeopardy.data[col_num-1].
+                            Questions[jeopardy.random_questions[col_num-1][row_num-1]].Answers[i].Selection;
+            var answer = answer_selection;
+            answer += ") ";
+            answer += jeopardy.data[col_num-1].
+                                Questions[jeopardy.random_questions[col_num-1][row_num-1]].Answers[i].Answer;
+            var query_string = "label[for='answer-" + answer_selection + "']";
+            $(query_string).text(answer);
+         }
+
+        // replace the question grid with the question and answers view
+        $('#question-box').css("display", "block");
+        $('#jeopardy-container').css("display", "none");
 
         // add the handler for clicking the submit button
         $("#final-answer").on( "click", jeopardy.CheckAnswer);
 
         // set timer
         jeopardy.timeout = setTimeout(function() {
-            jeopardy.time_remaining = 30;
+            jeopardy.time_remaining = jconstants.INITIAL_TIMER_SECS;
             jeopardy.setTimer();
-        }, 1000);
+        }, jconstants.ONE_SECOND_MS);
 
         // set attributes for the topic and question for element with id=the-question
-        $('#the-question').attr("topic", col_num);
-        $('#the-question').attr("question", row_num);
+        $('#the-question').attr("col_num", col_num);
+        $('#the-question').attr("row_num", row_num);
     }
 }
 
@@ -269,7 +274,7 @@ jeopardy.AskQuestion = function(box_element) {
 $(document).ready(function() {
 
     // Add column 1 Topic Questions
-    $(".jbox").click(function() {
+    $(".points").click(function() {
         jeopardy.AskQuestion($(this));
     });
 
